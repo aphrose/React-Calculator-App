0184edc5f9b520b63d52468b92f9ecc9fa4e8be2
aphrose <98501961+aphrose@users.noreply.github.com>
2022-04-29 09:44:11 +0900

    Create new project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './comp/Header';
+import Calculator from './comp/Calculator';
+import Footer from './comp/Footer';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Calculator />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/comp/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2022 홍경표. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
